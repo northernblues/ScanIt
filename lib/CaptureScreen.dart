28b32fc5ce47d9dc20c0032d1f03b94db5a3a3a2
VeyVovv <51308086+VeyVovv@users.noreply.github.com>
2022-02-07 13:17:38 +0545

    ScanIT App

---
+++ lib/CaptureScreen.dart
@@ -0,0 +1,308 @@
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:clipboard/clipboard.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:path_provider/path_provider.dart';
+import 'package:another_flushbar/flushbar.dart';
+import 'package:image_cropper/image_cropper.dart';
+// import 'package:edge_detection/edge_detection.dart';
+
+
+
+
+
+
+
+class CaptureScreen extends StatefulWidget {
+  const CaptureScreen({Key? key}) : super(key: key);
+
+
+  @override
+  _CaptureScreenState createState() => _CaptureScreenState();
+}
+
+class _CaptureScreenState extends State<CaptureScreen> {
+  String imagePath = 'asd';
+  String selectedItem = '';
+
+  String finalText = '';
+
+  late File pickedImage;
+  String imageName = '';
+
+  bool isImageLoaded = false;
+  final pdf = pw.Document();
+
+
+
+
+  getImageFromGallery() async {
+    if (selectedItem=='Scan Images from Gallery') {
+      final XFile? tempStore = await ImagePicker().pickImage(
+          source: ImageSource.gallery);
+
+
+      setState(() {
+        pickedImage = File(tempStore!.path);
+        isImageLoaded = true;
+        imagePath = tempStore.path.toString();
+        cropImage(pickedImage);
+
+
+      });
+    }
+
+    if (selectedItem=='Scan by Camera') {
+      final XFile? tempStore = await ImagePicker().pickImage(
+          source: ImageSource.camera);
+      // String? detected = (await EdgeDetection.detectEdge);
+
+
+
+      setState(() {
+        pickedImage = File(tempStore!.path);
+        isImageLoaded = true;
+        imagePath = tempStore.path.toString();
+        cropImage(pickedImage);
+        // imagePath= detected!;
+
+
+
+      });
+    }
+
+
+
+  }
+  cropImage(File picked) async{
+    File? cropped = await ImageCropper.cropImage(
+      androidUiSettings: const AndroidUiSettings(
+        toolbarColor: Colors.blue,
+        toolbarWidgetColor: Colors.white,
+        toolbarTitle: "Crop Image",
+      ),
+
+      sourcePath: picked.path,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9,
+      ],
+
+
+
+    );
+    setState(() {
+      pickedImage = cropped!;
+    });
+
+  }
+
+
+
+    Future getText(String path) async {
+    finalText = '';
+      final inputImage = InputImage.fromFile(pickedImage);
+      final textDetector = GoogleMlKit.vision.textDetector();
+      final RecognisedText _recognizedText = await textDetector.processImage(
+          inputImage);
+
+      for (TextBlock block in _recognizedText.blocks) {
+        for (TextLine textLine in block.lines) {
+          for (TextElement textElement in textLine.elements) {
+            setState(() {
+              finalText = finalText + ' ' + textElement.text;
+            });
+          }
+
+          finalText = finalText + "\n";
+        }
+      }
+    }
+
+
+  final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
+
+  @override
+
+  Widget build(BuildContext context)
+  {
+    selectedItem = ModalRoute.of(context)!.settings.arguments.toString();
+    return Scaffold(
+      key: _key,
+      appBar: AppBar(
+        title: Text(selectedItem,
+        style: GoogleFonts.atma(
+          fontSize: 15.0,
+          fontWeight: FontWeight.bold,
+        ),
+          ),
+        actions: [
+          TextButton(
+              onPressed: getImageFromGallery,
+              child: const Icon(
+                Icons.add_a_photo,
+                color: Colors.white,
+              ),
+
+          ),
+
+
+        IconButton(
+              icon: const Icon(Icons.copy),
+            onPressed: (){
+              FlutterClipboard.copy(finalText).then((value) => _key.currentState?..showSnackBar(const SnackBar(content: Text('Copied'))));
+
+          }, )
+        ],
+      ),
+
+
+
+      body: SingleChildScrollView(   //bottom overflowed by XX pixels NOT ANYMORE
+        child: Column(
+          children: [
+            const SizedBox(height: 100),
+            isImageLoaded?  Center(
+              child: Container(
+                height: 250.0,
+                width: 250.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: FileImage(pickedImage),
+                      fit: BoxFit.cover
+                ),
+              ),
+            )
+            ) : Container(),
+
+            const SizedBox(height: 60.0),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SelectableText(finalText,
+
+              ),
+            ),
+            // Center(
+            //   child: TextButton(
+            //     onPressed: (){
+            //       getImageFromGallery();
+            //       Future.delayed(const Duration(seconds: 6),(){
+            //         getText(imagePath);
+            //       });
+            //     },
+            //     child: Text(
+            //       "PickImage",
+            //       style: GoogleFonts.aBeeZee(
+            //         fontSize: 30,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            // Text(
+            //   finalText?? "This is a text",
+            // style: GoogleFonts.aBeeZee(
+            //   color:Colors.red,
+            // ),),
+
+
+          ],
+
+        ),
+      ),
+
+
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          getText(imagePath);
+
+        },
+        child: const Icon(
+          Icons.check
+        ),
+      ),
+
+
+        bottomNavigationBar: BottomAppBar(
+
+          child: IconButton(icon: const Icon(Icons.picture_as_pdf,
+            color: Colors.pink,
+
+          ), onPressed: () {
+            createPDF();
+            savePDF();
+            }),
+
+            ),
+
+    );
+
+  }
+
+  showPrintedMessage(String title, String msg) {
+    Flushbar(
+      title: title,
+      message: msg,
+      duration: const Duration(seconds: 3),
+      icon: const Icon(
+        Icons.info,
+        color: Colors.blue,
+
+      ),
+
+    ).show(context);
+  }
+
+  createPDF() async{
+
+      final image = pw.MemoryImage(
+      pickedImage.readAsBytesSync(),
+    );
+
+
+    pdf.addPage(pw.Page(pageFormat: PdfPageFormat.a4, build: (pw.Context context) {
+      return pw.Center(
+        child: pw.Image(image),
+
+
+      ); // Center
+    }));
+
+
+
+  }
+
+
+
+  savePDF() async{
+    String imageName = pickedImage.path.split('/').last;
+
+
+    try{
+
+        final dir = await getExternalStorageDirectory();
+        final file = File('${dir!.path}/$imageName.pdf');
+        await file.writeAsBytes(await pdf.save());
+        showPrintedMessage('success', 'saved to Documents');
+
+
+    }
+    catch(e){
+      showPrintedMessage('error', e.toString());
+
+    }
+
+
+  }
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:scanit/CaptureScreen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:scanit/PDFScreen.dart';
+
+
+void main() => runApp(const MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: HomeScreen(),
+));
+
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> itemList = ['Scan Images from Gallery', 'Scan by Camera'];
+  String selectedItem = '';
+
+
+  @override
+  Widget build(BuildContext context)
+  {
+    selectedItem = ModalRoute.of(context)!.settings.arguments.toString();
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SCANIt',
+          style: GoogleFonts.aladin(
+            fontSize: 27,
+
+          ),
+        ),
+        leading: IconButton(
+          icon: Image.asset('assets/ScanIt.png'),
+          onPressed: () { },
+        ),
+
+      ),
+
+
+      body: Container(
+          child: Center(
+        child: Column(
+          children: <Widget> [
+            Expanded(child: ListView.builder( //To wrap up Listview with Container > Center> Column> children <Widget> (to use BoxDecoration for Image) , use Expanded otherwise just inside body: Listview
+                padding: const EdgeInsets.only(top: 10, right: 10, left: 10),
+                itemCount: itemList.length,
+                itemBuilder: (context, index) {
+
+                  return Card(
+                    elevation: 0,
+                    child: ListTile(
+                      tileColor: Colors.blue,
+                      textColor: Colors.white,
+
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+
+
+                      title: Text(itemList[index],
+                        style: GoogleFonts.baloo(
+                        ),
+
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_forward,
+                        ),
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) => const CaptureScreen(),
+                            settings: RouteSettings(arguments: itemList[index]),
+                          ));
+                        },
+                        color: Colors.white,
+                      ),
+
+
+                      onTap: () {
+                      },
+
+                    ),
+
+
+                  );
+                }
+
+
+            ),),
+
+
+          ],
+        ),
+
+
+      ),
+
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage('assets/Flutter.png',
+
+            ),
+          ),
+          // border: Border.all(
+          //   color: Colors.black,
+          //   width: 8,
+          // ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+
+
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(
+            builder: (context) => const PDFScreen(),
+          ));
+        },
+        label: const Text('Image Collection to PDF'),
+        icon: const Icon(Icons.picture_as_pdf),
+        backgroundColor: Colors.pink,
+      ),
+
+      bottomNavigationBar: const BottomAppBar(
+
+        child: Center(
+          heightFactor: 4.0,
+
+          child: Text("This is ScanIt Application",
+        style: TextStyle(
+          fontSize: 10.0,
+          color: Colors.lightBlue,
+
+        ),
+        ),
+        ), ),
+
+
+
+
+    );
+  }
+}
