e09367694458d8a6074bbe63364af8bc7cc89fd2
northernblues <thenorthernblues@gmail.com>
2022-02-13 13:57:36 +0545

    added pdf

--- lib/CaptureScreen.dart
+++ lib/CaptureScreen.dart
@@ -220,51 +220,55 @@ class _CaptureScreenState extends State<CaptureScreen> {
 
     ).show(context);
   }
-}
+
+  createPDF() async{
+
+    final image = pw.MemoryImage(
+      pickedImage.readAsBytesSync(),
+    );
 
 
-
-createPDF() async{
-
-  final image = pw.MemoryImage(
-    pickedImage.readAsBytesSync(),
-  );
+    pdf.addPage(pw.Page(pageFormat: PdfPageFormat.a4, build: (pw.Context context) {
+      return pw.Center(
+        child: pw.Image(image),
 
 
-  pdf.addPage(pw.Page(pageFormat: PdfPageFormat.a4, build: (pw.Context context) {
-    return pw.Center(
-      child: pw.Image(image),
+      ); // Center
+    }));
 
 
-    ); // Center
-  }));
-
-
-
-}
-
-
-
-savePDF() async{
-  String imageName = pickedImage.path.split('/').last;
-
-
-  try{
-
-    final dir = await getExternalStorageDirectory();
-    final file = File('${dir!.path}/$imageName.pdf');
-    await file.writeAsBytes(await pdf.save());
-    showPrintedMessage('success', 'saved to Documents');
-
-
-  }
-  catch(e){
-    showPrintedMessage('error', e.toString());
 
   }
 
 
+
+  savePDF() async{
+    String imageName = pickedImage.path.split('/').last;
+
+
+    try{
+
+      final dir = await getExternalStorageDirectory();
+      final file = File('${dir!.path}/$imageName.pdf');
+      await file.writeAsBytes(await pdf.save());
+      showPrintedMessage('success', 'saved to Documents');
+
+
+    }
+    catch(e){
+      showPrintedMessage('error', e.toString());
+
+    }
+
+
+  }
+
+
 }
+
+
+
+
 
 
 
